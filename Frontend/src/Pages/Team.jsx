7318b54c1db233f02e9a0ad47a6785e89cf112ae
Team-Hackthon <hackthonteam06@gmail.com>
2026-09-04 20:50:45 +0530

    Redesign ScamShield cyber background

--- Frontend/src/Pages/Team.jsx
+++ Frontend/src/Pages/Team.jsx
@@ -5,7 +5,7 @@ const teamMembers = [
     name: "Mann Patel",
     role: "Backend Developer",
     about: "Building secure systems that power ScamShield behind the scenes.",
-    image: "/team/mann.jpg",
+    image: "/team/Mann.jpg",
   },
   {
     name: "Jaina Patel",
